254ce9c2b8cb9214ad0f9d963c4402d29fb0e2b9
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2023-01-26 21:52:35 -0300

    refactor: change contex folder name to Data and update imports

--- Data/PaymentApiContext.cs
+++ Data/PaymentApiContext.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace Payment_API.Context
+namespace Payment_API.Data
 {
     public class PaymentApiContext : DbContext
     {
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Payment_API.Context;
+using Payment_API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
