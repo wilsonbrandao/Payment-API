2ac420e2f08fcd80b764bbc396c4ddee4ad13ea4
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2023-01-26 22:23:50 -0300

    feat: create Product model

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Payment_API.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "'Name' field is required")]
+        [StringLength(100, ErrorMessage = "The name field cannot exceed 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "'ProductAmount' field is required")]
+        public int ProductAmount { get; set; }
+    }
+}
