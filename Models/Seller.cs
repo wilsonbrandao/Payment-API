f23f52b327bccd30f92917e75d0d4d9a9056976d
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2023-01-26 22:16:55 -0300

    feat: create Seller model

---
+++ Models/Seller.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Payment_API.Models
+{
+    public class Seller
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="CPF field is required")]
+        [StringLength(11, ErrorMessage = "CPF field cannot exceed 11 characters")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "CPF must be numeric")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "NAME field is required")]
+        [StringLength(100, ErrorMessage = "The name field cannot exceed 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "EMAIL field is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "PHONENUMBER field is required")]
+        public string PhoneNumber { get; set; }
+    }
+}
