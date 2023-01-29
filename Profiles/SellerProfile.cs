e3b1782fa64069ca482a006d097587d9d5dbb747
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2023-01-28 23:36:33 -0300

    feat: create Seller Profile

---
+++ Profiles/SellerProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Payment_API.Data.DTOs.Seller;
+using Payment_API.Models;
+
+namespace Payment_API.Profiles
+{
+    public class SellerProfile : Profile
+    {
+        public SellerProfile()
+        {
+            CreateMap<CreateSellerDto, Seller>();
+            CreateMap<UpdateSellerDto, Seller>();
+            CreateMap<Seller, ReadSellerDto>();
+        }
+    }
+}
