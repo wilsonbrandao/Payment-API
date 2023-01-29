e938cea7856897069309f9e2c8d9464878923613
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2023-01-28 23:31:37 -0300

    feat: create Seller Dtos

---
+++ Data/DTOs/Seller/CreateSellerDto.cs
@@ -0,0 +1,24 @@
+﻿using Payment_API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Payment_API.Data.DTOs.Seller
+{
+    public class CreateSellerDto
+    {
+        [Required(ErrorMessage = "'Cpf' field is required")]
+        [StringLength(11, ErrorMessage = "'Cpf' field cannot exceed 11 characters")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "'Cpf' must be numeric")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "'Name' field is required")]
+        [StringLength(100, ErrorMessage = "'Name' field cannot exceed 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "'Email' field is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "'PhoneNumber' field is required")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Data/DTOs/Seller/ReadSellerDto.cs
@@ -0,0 +1,15 @@
+﻿using Payment_API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Payment_API.Data.DTOs.Seller
+{
+    public class ReadSellerDto
+    {
+        public int Id { get; set; }
+        public string Cpf { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
