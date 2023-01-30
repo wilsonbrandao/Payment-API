c757a30ed0c88ffc286a6b80e755bb96fa6f20d0
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2023-01-29 21:49:21 -0300

    feat: create SaleController with basic CRUD methods

--- Controllers/SaleController.cs
+++ Controllers/SaleController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Payment_API.Data;
+using Payment_API.Models;
 
 namespace Payment_API.Controllers
 {
@@ -22,5 +23,81 @@ namespace Payment_API.Controllers
         {
             return Ok(_context.Sales);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSaleById(int id)
+        {
+            Sale saleDatabase = _context.Sales.Where(saleQuery => saleQuery.Id == id).FirstOrDefault();
+            if(saleDatabase == null) return NotFound();
+            return Ok(saleDatabase);
+        }
+
+        [HttpPost]
+        public IActionResult RegisterSale([FromBody] Sale createSale)
+        {
+            Sale sale = new Sale()
+            {
+                IdSeller = createSale.IdSeller,
+                StatusSale = createSale.StatusSale,
+            };
+            _context.Sales.Add(sale);
+            _context.SaveChanges();
+
+            List <Transaction> transactions = createSale.Transactions.ToList();
+            foreach(Transaction transaction in transactions)
+            {
+                transaction.SaleId = sale.Id;
+                _context.Transactions.Add(transaction);
+                _context.SaveChanges();
+            }
+
+            return CreatedAtAction(nameof(GetSaleById), new {Id = sale.Id}, sale);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateSale(int id, [FromBody] Sale updateSale)
+        {
+            Sale saleDatabase = _context.Sales.Where (saleQuery => saleQuery.Id == id).FirstOrDefault();
+            if(saleDatabase ==null) return NotFound();
+
+            List<Transaction> transactionsDataBase = _context.Transactions.Where(transactionQuery => transactionQuery.SaleId == id).ToList();
+            foreach (Transaction transactionDatabase in transactionsDataBase)
+            {
+                _context.Transactions.Remove(transactionDatabase);
+            }
+
+            List<Transaction> transactions = updateSale.Transactions.ToList();
+            foreach (Transaction transaction in transactions)
+            {
+                transaction.SaleId = saleDatabase.Id;
+                _context.Transactions.Add(transaction);
+            }
+
+            saleDatabase.IdSeller = updateSale.IdSeller;
+            saleDatabase.StatusSale = updateSale.StatusSale;
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteSale(int id)
+        {
+            Sale saleDatabase = _context.Sales.Where(saleQuery => saleQuery.Id == id).FirstOrDefault();
+            if (saleDatabase == null) return NotFound();
+
+            List<Transaction> transactionsDataBase = _context.Transactions.Where(transactionQuery => transactionQuery.SaleId == id).ToList();
+            foreach (Transaction transactionDatabase in transactionsDataBase)
+            {
+                _context.Transactions.Remove(transactionDatabase);
+            }
+
+            _context.Sales.Remove(saleDatabase);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
     }
 }
